07181e73c2dc6a485e0bdeb04e9a1592a615602d
yogihj <yogihj@gmail.com>
2018-11-26 15:40:18 -0500

    Merge branch 'master' of https://github.com/emizhu/CS171-Final
    # Conflicts:
    #	css/style.css

--- js/resource.js
+++ js/resource.js
@@ -4,3 +4,6 @@ var ATUS_Lexicon = [ "American Time Use Survey Activity Lexicon",
     "This document is the activity classification scheme (including examples) used to code\n" +
     "activities collected during the American Time Use Survey into time-use categories.",
     src= "https://www.bls.gov/tus/lexiconwex2017.pdf"];
+
+
+var ATUS_Average =[ "Average Time" ,src = "https://www.bls.gov/news.release/atus.t01.htm"];
